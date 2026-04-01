070a99b1bf130a307673167e5c39ed1d79b5bba6
Artmines <arturowells@gmail.com>
2026-04-01 06:33:04 -0230

    Version checker addition

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,19 +1,22 @@
 fx_version 'cerulean'
 games { 'gta5' }
 lua54 'yes'
+
+version '1.0.0'
+repository 'https://github.com/Mythic-Framework/mythic-menu'
+
 client_script "@mythic-base/components/cl_error.lua"
 client_script "@mythic-pwnzor/client/check.lua"
-
-ui_page "ui/html/index.html"
-
-files {
-    "ui/html/*.*",
-}
 
 client_scripts {
     'client/*.lua'
 }
-
 server_scripts {
     'server/*.lua'
 }
+
+
+files {
+    "ui/html/*.*",
+}
+ui_page "ui/html/index.html"
